a66baa2483e255d3b67e8ad177ed3d73a0b2a755
enpitsulin <lldslm@vip.qq.com>
2022-04-15 16:46:39 +0800

    add testing coverage

--- test.js
+++ test.js
@@ -74,6 +74,63 @@ test('toggle', (t) => {
   t.end()
 })
 
+test('toggle witch force', (t) => {
+  const el = h('p.center', 'some stuff')
+  const expected = ClassList(el)
+
+  t.ok(expected.toggle('center', true))
+  t.ok(expected.contains('center'))
+  t.notOk(expected.toggle('center', false))
+  t.notOk(expected.contains('center'))
+
+  t.ok(expected.toggle('center', () => true))
+  t.ok(expected.contains('center'))
+  t.notOk(expected.toggle('center', () => false))
+  t.notOk(expected.contains('center'))
+
+  t.equal(expected.length, 0)
+
+  t.end()
+})
+
+test('support', (t) => {
+  const el = h('p.center', 'some stuff')
+  const expected = ClassList(el)
+  expected.supports()
+  t.pass('support')
+  t.end()
+})
+
+test('entries', (t) => {
+  const el = h('p.center.top', 'some stuff')
+  const expected = ClassList(el)
+
+  t.notStrictEqual(
+    [...expected.entries()],
+    [
+      [0, 'center'],
+      [1, 'top']
+    ]
+  )
+  t.end()
+})
+
+test('keys', (t) => {
+  const el = h('p.center.top', 'some stuff')
+  const expected = ClassList(el)
+
+  t.notStrictEqual([...expected.keys()], [0, 1])
+  t.end()
+})
+
+test('values', (t) => {
+  const el = h('p.center.top', 'some stuff')
+  const expected = ClassList(el)
+
+  t.notStrictEqual([...expected.values()], ['center', 'top'])
+  t.end()
+})
+
 test('replace', (t) => {
   const el = h('p.center', 'some stuff')
   const expected = ClassList(el)
